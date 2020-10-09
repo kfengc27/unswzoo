b73a90cab84552a728febeb47a7ef173691856da
Chengbin <kfengc27@gmail.com>
2020-10-10 10:44:07 +1100

    add files

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("你好哇， 这是UNSWZoo的首页, 还在开发哦。校内论坛请转 <a>bbs.unswzoo.com</a>") 
+    return HttpResponse("Hi, Welcome to UNSW Zoo home page. If you want to visit, please see.") 
 
