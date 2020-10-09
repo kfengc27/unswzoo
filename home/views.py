2d573117f8716e6944702525f76dfa308cce363e
Chengbin <kfengc27@gmail.com>
2020-10-10 10:52:25 +1100

    update zoo

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi, Welcome to UNSW Zoo home page. If you want to visit, please see.") 
+    return HttpResponse("Hi, Welcome to UNSW Zoo home page. If you want to visit, please see bbs.unswzoo.com") 
 
