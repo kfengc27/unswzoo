cfa8a2e428471e7064c383bff6eb80380dc41c01
Chengbin <kfengc27@gmail.com>
2020-10-10 10:24:58 +1100

    add files

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.") 
+    return HttpResponse("你好哇， 这是UNSWZoo的首页, 还在开发哦。校内论坛请转 <a>bbs.unswzoo.com</a>") 
 
