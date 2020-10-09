cbc9e4f8fb713c767b864803a95adccf238fbc83
Chengbin <kfengc27@gmail.com>
2020-10-09 22:24:02 +1100

    init django files for heroku

--- unswzoo/settings.py
+++ unswzoo/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
